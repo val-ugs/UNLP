785336e5b8674fd67ae24a6d4b886dfb926b260e
val-ugs <valentin.1999@mail.ru>
2024-03-18 02:13:25 +0300

    feat (frontend): add nlp dataset and hugging face model nodes

--- frontend/src/pages/NlpConstructorPage/nodes/HuggingFaceModelNode/index.tsx
+++ frontend/src/pages/NlpConstructorPage/nodes/HuggingFaceModelNode/index.tsx
@@ -1,21 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Handle, Position } from 'reactflow';
 import './styles.scss';
+import LabeledElement from 'components/interstitial/LabeledElement';
+import Select, { SelectProps } from 'components/common/Select';
+import {
+  HuggingFaceModelProps,
+  HuggingFaceModelType,
+} from 'interfaces/huggingFaceModel.interface';
+import { huggingFaceModelApi } from 'services/huggingFaceModelService';
+import { skipToken } from '@reduxjs/toolkit/query';
+import Button from 'components/common/Button';
+import { enumToArray } from 'helpers/enumToArray';
 
 const HuggingFaceModelNode = ({ data }) => {
+  const [huggingFaceModelId, setHuggingFaceModelId] = useState<number>();
+  const [huggingFaceModelType, setHuggingFaceModelType] =
+    useState<HuggingFaceModelType>(HuggingFaceModelType.Classification);
+  const [huggingFaceModels, setHuggingFaceModels] =
+    useState<HuggingFaceModelProps[]>();
+  const {
+    data: huggingFaceModelsData,
+    isLoading: huggingFaceModelsLoading,
+    isError,
+  } = huggingFaceModelApi.useGetHugginsFaceModelsByTypeQuery(
+    huggingFaceModelType ?? skipToken
+  );
+
+  useEffect(() => {
+    if (huggingFaceModelsData) setHuggingFaceModels(huggingFaceModelsData);
+    if (isError) setHuggingFaceModels(undefined);
+  }, [huggingFaceModelsData, isError]);
+
+  const setHuggingFaceModelValue = (value: number) => {
+    const huggingFaceModel = huggingFaceModels?.find((hfm) => hfm.id == value);
+    if (huggingFaceModel) setHuggingFaceModelId(huggingFaceModel.id);
+  };
+  const huggingFaceModelSelect: SelectProps<number> = {
+    className: 'hugging-face-model-node__select nodrag nowheel',
+    selectedValue: huggingFaceModelId ?? 0,
+    setSelectedValue: setHuggingFaceModelValue,
+    children: huggingFaceModels?.map(
+      (huggingFaceModel: HuggingFaceModelProps) => {
+        return (
+          <Select.Item key={huggingFaceModel.id} value={huggingFaceModel.id}>
+            {huggingFaceModel.name}
+          </Select.Item>
+        );
+      }
+    ),
+  };
+
   return (
-    <div className="hugging-face-model-node">
-      <div className="hugging-face-model-node__name">Hugging face model</div>
+    <div className="hugging-face-model-node nopan">
       <Handle
         className="hugging-face-model-node__handle"
         type={'target'}
         position={Position.Left}
       />
-      <Handle
-        className="hugging-face-model-node__handle"
-        type={'source'}
-        position={Position.Right}
-      />
+      <div className="hugging-face-model-node__main">
+        <div className="hugging-face-model-node__main-item">
+          <div className="hugging-face-model-node__type">
+            {enumToArray(HuggingFaceModelType).map(
+              (hfmt: HuggingFaceModelType) => (
+                <div className="hugging-face-model-node__type-item" key={hfmt}>
+                  <Button
+                    className={`hugging-face-model-node__type-button ${
+                      hfmt == huggingFaceModelType ? 'active' : ''
+                    }`}
+                    onClick={() => {
+                      setHuggingFaceModelType(hfmt);
+                      setHuggingFaceModelId(0);
+                    }}
+                  >
+                    {hfmt}
+                  </Button>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+        <div className="hugging-face-model-node__main-item">
+          {huggingFaceModels ? (
+            <>
+              <LabeledElement
+                className="hugging-face-model-node__labeled-element"
+                labelElement={{ value: 'Hugging face model' }}
+              >
+                <Select
+                  className={huggingFaceModelSelect.className}
+                  selectedValue={huggingFaceModelSelect.selectedValue}
+                  setSelectedValue={huggingFaceModelSelect.setSelectedValue}
+                  disabled={huggingFaceModelSelect.disabled}
+                >
+                  {huggingFaceModelSelect.children}
+                </Select>
+              </LabeledElement>
+              <Handle
+                className="hugging-face-model-node__handle"
+                type={'source'}
+                position={Position.Right}
+              />
+            </>
+          ) : (
+            'Hugging face models not found'
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/NlpConstructorPage/nodes/NlpDatasetNode/index.tsx
+++ frontend/src/pages/NlpConstructorPage/nodes/NlpDatasetNode/index.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Handle, Position } from 'reactflow';
+import LabeledElement from 'components/interstitial/LabeledElement';
+import Select, { SelectProps } from 'components/common/Select';
+import { nlpDatasetApi } from 'services/nlpDatasetService';
+import { NlpDatasetProps } from 'interfaces/nlpDataset.interface';
 import './styles.scss';
 
-const NlpDatasetNode = ({ data }) => {
+const NlpDatasetNode = () => {
+  const [nlpDatasetId, setNlpDatasetId] = useState<number>();
+  const [nlpDatasets, setNlpDatasets] = useState<NlpDatasetProps[]>([]);
+  const {
+    data: nlpDatasetsData,
+    error,
+    isLoading: nlpDatasetsLoading,
+  } = nlpDatasetApi.useGetNlpDatasetsQuery();
+  useEffect(() => {
+    if (nlpDatasetsData) setNlpDatasets(nlpDatasetsData);
+  }, [nlpDatasetsData]);
+
+  const setNlpDatasetIdValue = (value: number) => {
+    setNlpDatasetId(value);
+  };
+  const nlpDatasetSelect: SelectProps<number> = {
+    className: 'nlp-dataset-node__select nodrag nowheel',
+    selectedValue: nlpDatasetId ?? 0,
+    setSelectedValue: setNlpDatasetIdValue,
+    children: nlpDatasets?.map((nlpDataset: NlpDatasetProps) => {
+      return (
+        <Select.Item key={nlpDataset.id} value={nlpDataset.id}>
+          {`Dataset ${nlpDataset.id}`}
+        </Select.Item>
+      );
+    }),
+  };
+
   return (
-    <div className="nlp-dataset-node">
-      <div className="nlp-dataset-node__name">Nlp Dataset</div>
+    <div className="nlp-dataset-node nopan">
+      <div className="nlp-dataset-node__main">
+        <LabeledElement
+          className="nlp-dataset-node__labeled-element"
+          labelElement={{ value: 'Nlp dataset' }}
+        >
+          <Select
+            className={nlpDatasetSelect.className}
+            selectedValue={nlpDatasetSelect.selectedValue}
+            setSelectedValue={nlpDatasetSelect.setSelectedValue}
+            disabled={nlpDatasetSelect.disabled}
+          >
+            {nlpDatasetSelect.children}
+          </Select>
+        </LabeledElement>
+      </div>
       <Handle
         className="nlp-dataset-node__handle"
         type={'source'}
