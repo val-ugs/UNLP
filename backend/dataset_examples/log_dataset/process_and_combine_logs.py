09efd86ab50ef2f95b6e6aa5bd577b45bb0d6e07
val-ugs <valentin.1999@mail.ru>
2024-04-14 23:40:08 +0300

    feat (backend/examples): add generate train, valid, test log datasets

--- backend/dataset_examples/log_dataset/process_and_combine_logs.py
+++ backend/dataset_examples/log_dataset/process_and_combine_logs.py
@@ -33,50 +33,114 @@ def convert_to_my_dataset():
     token_pattern_to_remove = ''
     token_pattern_to_split = '[\\s,\\[,\\],\\@,\\:,\\-,\\=, \\\, \\/]'
 
-    nlp_texts = []
-
     apache_log_nlp_dataset_parser = ApacheLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(apache_log_nlp_dataset_parser.process()[0:200])
+    apache_nlp_texts = apache_log_nlp_dataset_parser.process()
 
     hadoop_log_nlp_dataset_parser = HadoopLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(hadoop_log_nlp_dataset_parser.process()[0:200])
+    hadoop_nlp_texts = hadoop_log_nlp_dataset_parser.process()
 
     hdfs_log_nlp_dataset_parser = HDFSLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(hdfs_log_nlp_dataset_parser.process()[0:200])
+    hdfs_nlp_texts = hdfs_log_nlp_dataset_parser.process()
 
     linux_log_nlp_dataset_parser = LinuxLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(linux_log_nlp_dataset_parser.process()[0:200])
+    linux_nlp_texts = linux_log_nlp_dataset_parser.process()
 
     mac_log_nlp_dataset_parser = MacLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(mac_log_nlp_dataset_parser.process()[0:200])
+    mac_nlp_texts = mac_log_nlp_dataset_parser.process()
 
     open_ssh_log_nlp_dataset_parser = OpenSSHLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(open_ssh_log_nlp_dataset_parser.process()[0:200])
+    open_ssh_nlp_texts = open_ssh_log_nlp_dataset_parser.process()
 
     proxifier_log_nlp_dataset_parser = ProxifierLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(proxifier_log_nlp_dataset_parser.process()[0:200])
+    proxifier_nlp_texts = proxifier_log_nlp_dataset_parser.process()
 
     spark_log_nlp_dataset_parser = SparkLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(spark_log_nlp_dataset_parser.process()[0:200])
+    spark_nlp_texts = spark_log_nlp_dataset_parser.process()
 
     windows_log_nlp_dataset_parser = WindowsLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(windows_log_nlp_dataset_parser.process()[0:200])
+    windows_nlp_texts = windows_log_nlp_dataset_parser.process()
 
     zookeeper_log_nlp_dataset_parser = ZookeeperLogNlpDatasetParser(token_pattern_to_remove, token_pattern_to_split, ner_labels)
-    nlp_texts.extend(zookeeper_log_nlp_dataset_parser.process()[0:200])
+    zookeeper_nlp_texts = zookeeper_log_nlp_dataset_parser.process()
 
-    print(len(nlp_texts))
+    
+    train_nlp_texts = []
+    train_nlp_texts.extend(apache_nlp_texts[0:200])
+    train_nlp_texts.extend(hadoop_nlp_texts[0:200])
+    train_nlp_texts.extend(hdfs_nlp_texts[0:200])
+    train_nlp_texts.extend(linux_nlp_texts[0:200])
+    train_nlp_texts.extend(mac_nlp_texts[0:200])
+    train_nlp_texts.extend(open_ssh_nlp_texts[0:200])
+    train_nlp_texts.extend(proxifier_nlp_texts[0:200])
+    train_nlp_texts.extend(spark_nlp_texts[0:200])
+    train_nlp_texts.extend(windows_nlp_texts[0:200])
+    train_nlp_texts.extend(zookeeper_nlp_texts[0:200])
 
-    json_data = {
-        'nlp_texts': nlp_texts,
+    print(len(train_nlp_texts))
+    random.shuffle(train_nlp_texts)
+
+    train_json_data = {
+        'nlp_texts': train_nlp_texts,
         'ner_labels': ner_labels,
         'token_pattern_to_remove': token_pattern_to_remove,
         'token_pattern_to_split': token_pattern_to_split
     }
 
-    json_file_path = join(dirname(abspath(__file__)), "my_log_dataset.json")
-    with open(json_file_path, 'w') as f:
-        json.dump(json_data, f)
+    train_json_file_path = join(dirname(abspath(__file__)), "my_train_log_dataset.json")
+    with open(train_json_file_path, 'w') as f:
+        json.dump(train_json_data, f)
+
+    valid_nlp_texts = []
+    valid_nlp_texts.extend(apache_nlp_texts[200:240])
+    valid_nlp_texts.extend(hadoop_nlp_texts[200:240])
+    valid_nlp_texts.extend(hdfs_nlp_texts[200:240])
+    valid_nlp_texts.extend(linux_nlp_texts[200:240])
+    valid_nlp_texts.extend(mac_nlp_texts[200:240])
+    valid_nlp_texts.extend(open_ssh_nlp_texts[200:240])
+    valid_nlp_texts.extend(proxifier_nlp_texts[200:240])
+    valid_nlp_texts.extend(spark_nlp_texts[200:240])
+    valid_nlp_texts.extend(windows_nlp_texts[200:240])
+    valid_nlp_texts.extend(zookeeper_nlp_texts[200:240])
+
+    print(len(valid_nlp_texts))
+    random.shuffle(valid_nlp_texts)
+
+    valid_json_data = {
+        'nlp_texts': valid_nlp_texts,
+        'ner_labels': ner_labels,
+        'token_pattern_to_remove': token_pattern_to_remove,
+        'token_pattern_to_split': token_pattern_to_split
+    }
+
+    valid_json_file_path = join(dirname(abspath(__file__)), "my_valid_log_dataset.json")
+    with open(valid_json_file_path, 'w') as f:
+        json.dump(valid_json_data, f)
+
+    test_nlp_texts = []
+    test_nlp_texts.extend(apache_nlp_texts[240:280])
+    test_nlp_texts.extend(hadoop_nlp_texts[240:280])
+    test_nlp_texts.extend(hdfs_nlp_texts[240:280])
+    test_nlp_texts.extend(linux_nlp_texts[240:280])
+    test_nlp_texts.extend(mac_nlp_texts[240:280])
+    test_nlp_texts.extend(open_ssh_nlp_texts[240:280])
+    test_nlp_texts.extend(proxifier_nlp_texts[240:280])
+    test_nlp_texts.extend(spark_nlp_texts[240:280])
+    test_nlp_texts.extend(windows_nlp_texts[240:280])
+    test_nlp_texts.extend(zookeeper_nlp_texts[240:280])
+
+    print(len(test_nlp_texts))
+    random.shuffle(test_nlp_texts)
+
+    test_json_data = {
+        'nlp_texts': test_nlp_texts,
+        'ner_labels': ner_labels,
+        'token_pattern_to_remove': token_pattern_to_remove,
+        'token_pattern_to_split': token_pattern_to_split
+    }
+
+    test_json_file_path = join(dirname(abspath(__file__)), "my_test_log_dataset.json")
+    with open(test_json_file_path, 'w') as f:
+        json.dump(test_json_data, f)
     
 
 convert_to_my_dataset()
